40b33e04e8da166db57891e2733baad1d25f9c27
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2024-12-16 19:06:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const table = document.querySelector(".table");
 const tbody = document.querySelector("#output");
 
 const wait = function (id) {
+  const time = Math.random() * 2 + 1;
   return new Promise(function (resolve, _) {
-    const time = Math.random() * 2 + 1;
     setTimeout(function () {
       resolve({ time, id });
     }, time * 1000);
@@ -12,19 +12,18 @@ const wait = function (id) {
 
 const threePromise = [wait("first"), wait("second"), wait("third")];
 
+const start = new Date();
 Promise.all(threePromise).then((value) => {
+  const end = new Date();
   tbody.removeChild(tbody.firstElementChild);
-  const sum = renderRow(value);
-  let html = `<tr><td>Total</td><td>${sum}</td></tr>`;
+  renderRow(value);
+  let html = `<tr><td>Total</td><td>${(end - start) / 1000}</td></tr>`;
   tbody.insertAdjacentHTML("beforeend", html);
 });
 
 function renderRow(value) {
-  let sum = 0;
   for (let i = 0; i < 3; i++) {
-    sum += value[i].time;
     let html = `<tr><td>Promise ${i + 1}</td><td>${value[i].time}</td></tr>`;
     tbody.insertAdjacentHTML("beforeend", html);
   }
-  return sum;
 }
